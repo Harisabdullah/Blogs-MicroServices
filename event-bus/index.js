80f9eb23ea1f77baaa9015db1a381187e41e3914
harisabdullah <hariskhan1435@gmail.com>
2022-10-28 16:25:22 +0500

    Adding changes

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,17 +5,27 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {
+
+
+app.post('/events', async (req, res) => {
   const event = req.body;
 
-  axios.post('http://localhost:4000/events', event);
-  axios.post('http://localhost:4001/events', event);
-  axios.post('http://localhost:4002/events', event);
+  console.log('echoing ...');
+
+  try{
+    await axios.post('http://localhost:4000/events', event);
+    await axios.post('http://localhost:4001/events', event);
+    await axios.post('http://localhost:4002/events', event);
+
+  } catch (e){
+    console.log("error echoing events");
+  }
+
 
   res.send({ status: 'OK' });
 })
 
 app.listen(4005, ()=> {
-  console.log('Listening on Port: 4005');
+  console.log('Event-bus Listening on Port: 4005');
 })
 
